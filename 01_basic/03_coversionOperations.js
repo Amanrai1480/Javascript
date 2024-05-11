7e3ddce9398f91580b08eb18d6dff7550c486b91
Aman Rai <amanrai6266@gmail.com>
2024-05-11 11:12:21 +0000

    conversion of datatypes

--- 01_basic/03_coversionOperations.js
+++ 01_basic/03_coversionOperations.js
@@ -21,3 +21,29 @@ let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// *************************operation***********
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%2);
+
+let str1="Hello"
+let str2=" Aman"
+let str3=str1+str2
+console.log(str3); //Hello Aman
+
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log("1"+2+2); //122
+console.log(1+2+"2"); //32
+
+console.log(+true); //1
+console.log(+"");   //0
+
+let value=5
+value++
+console.log(value);
