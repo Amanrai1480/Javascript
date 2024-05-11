92a20cfa74685e231c0f409d77bbef103fc38528
Aman Rai <amanrai6266@gmail.com>
2024-05-11 10:27:43 +0000

    dataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" //treat all js code as newer version
+//alert(3+3) it will give error as it is nodejs not browser
+//for documentation go for "mdn" and tc39
+
+let age=22
+let name="aman"
+let isValid=true
+
+console.log(typeof age);  //number
+console.log(typeof "aman") //string
+console.log(typeof null);  //*object*
+console.log(typeof undefined);  //undefined
+          
+          //primitive data types:
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned yet
+//symbol => uniqueness
+
+          //
