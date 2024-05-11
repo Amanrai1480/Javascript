cd4c712d253c9ccd729f8d9fd194d790689ae6b4
Aman Rai <amanrai6266@gmail.com>
2024-05-11 10:27:23 +0000

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,18 @@
+const accountId=14456;
+let accountEmail="amanrai123@gmail.com";
+var accountPassword="aman123";
+accountCity="bhopal";
+
+//accountId=234; not allowed, to change the const.
+
+accountEmail="aman23@gmail.com";
+accountPassword="123";
+accountCity="indore";
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/*we will prefer to use let or const
+var: Variable can be re-declared and updated. A global scope variable
+let: variable can't be re-declared but can be updated. A block scope variable
+const:  variable can't be re-declared or can't be updated.*/
