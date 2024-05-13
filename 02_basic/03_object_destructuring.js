c829b07159dc21863cc5b6a7bca348c4e932018b
Aman Rai <amanrai6266@gmail.com>
2024-05-13 09:24:44 +0000

    object_destructuring

---
+++ 02_basic/03_object_destructuring.js
@@ -0,0 +1,25 @@
+// ***************object destructuring*****************************8
+
+const course={
+    courseName:"js",
+    price:99,
+    courseInstructor:"Aman"
+}
+ const {courseInstructor:inst}=course
+ console.log(inst);
+
+ // ********json format**********************
+ /*
+{
+
+}
+or
+[
+    {},
+    {},
+    {}
+]
+
+
+
+ */
