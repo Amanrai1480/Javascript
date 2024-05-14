e4e06439ccfa9a7604e7f02552695f5f73c93cc2
Aman Rai <amanrai6266@gmail.com>
2024-05-14 16:35:32 +0000

    forEach

--- 05_iterations/forEach_loop.js
+++ 05_iterations/forEach_loop.js
@@ -38,4 +38,14 @@ console.log(val,index,arr);
  })
 
 
+ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const newNums = []
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// } )
+// console.log(newNums);
+
+
 
