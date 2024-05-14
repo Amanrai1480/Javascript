8899510cfa8b896cba77e1f259eaf56b49dcbeae
Aman Rai <amanrai6266@gmail.com>
2024-05-14 07:46:27 +0000

    for in loop

---
+++ 05_iterations/for_in_loop.js
@@ -0,0 +1,29 @@
+//for of loop not works in iterating the object so we use for in loop for objects and for of and forEach for array
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+for (const key in map) {
+   
+    console.log(key);
+}
