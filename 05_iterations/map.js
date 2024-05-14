c162e228968371cb9ac8add6405c88496a2d4f01
Aman Rai <amanrai6266@gmail.com>
2024-05-14 16:57:02 +0000

    reduce

---
+++ 05_iterations/map.js
@@ -0,0 +1,38 @@
+// ******Array.prototype.map()*************
+//The map() method of Array instances creates a new array populated with the results of calling a provided function on every element in the calling array.
+const array1 = [1, 4, 9, 16];
+
+// Pass a function to map
+const map1 = array1.map((x) => x * 2);
+
+console.log(map1);
+// Expected output: Array [2, 8, 18, 32]
+
+
+const kvArray = [
+    { key: 1, value: 10 },
+    { key: 2, value: 20 },
+    { key: 3, value: 30 },
+  ];
+  
+  const reformattedArray = kvArray.map(({ key, value }) => ({ [key]: value }));
+  
+  console.log(reformattedArray); // [{ 1: 10 }, { 2: 20 }, { 3: 30 }]
+  console.log(kvArray);
+  // [
+  //   { key: 1, value: 10 },
+  //   { key: 2, value: 20 },
+  //   { key: 3, value: 30 }
+  // ]
+
+  // ************chaining********************
+  const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
